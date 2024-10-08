9b4d49c0e2532ec5899b89cfeed5d057e2ed1d18
DjoninhaA <djonathan8016@gmail.comgit>
2024-10-08 20:18:11 -0300

    instalando apps no app-root

--- app_delivery/settings.py
+++ app_delivery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'produto'
+    'produto.apps.ProdutoConfig',
+    'pedido.apps.PedidoConfig'
 ]
 
 MIDDLEWARE = [
