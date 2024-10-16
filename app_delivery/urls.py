b2ffa2e39a415bfd56d87c774db731d8c26bf347
DjoninhaA <djonathan8016@gmail.comgit>
2024-10-16 14:14:35 -0300

    alterando endereço de rotas

--- app_delivery/urls.py
+++ app_delivery/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('produto/', include('produto.urls')),
     path('pedido/', include('pedido.urls')),
-    path('bot/', include('bot.urls')),
+    path('', include('bot.urls')),
 ]
--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('config', views.config_bot, name = 'config_bot'),
+    path('', views.home_bot, name = 'home-bot'),
+    path('config', views.config_bot, name = 'config_bot')
 ]
