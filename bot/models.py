a5a371df0ec87cf39a74bd1f04be46691e903bdc
DjoninhaA <djonathan8016@gmail.comgit>
2024-10-16 10:34:02 -0300

    feat- criando models bot

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Bot(models.Model):
+
+    bot_name = models.CharField(primary_key=True, max_length=100, default='')
+    bot_description = models.CharField(max_length=180, default='')
+
+
+    def __str__(self):
+        return f'BotName: {self.bot_name} | Description: {self.bot_description}'
