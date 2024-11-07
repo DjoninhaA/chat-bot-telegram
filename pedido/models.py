f08a5424af19504a49e68a068d4e3cdd51dfc806
DjoninhaA <djonathan8016@gmail.comgit>
2024-11-06 21:06:27 -0300

    feat: criando o model de pedidos

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
 from django.db import models
+from produto.models import Produto 
 
-# Create your models here.
+class Pedido(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    #cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f'ID:{self.id} | Produto: {self.produto.nome} | Quantidade {self.quantidade}'
