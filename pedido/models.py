672a8d57e8e7fd2130721d8b5c8bf200cf686dbc
DjoninhaA <djonathan8016@gmail.comgit>
2024-11-11 20:28:55 -0300

    feat: criando model de Pedido

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from .models import Produto
 
-# Create your models here.
+ORDER_STATUS = ((0, 'AGUARDANDO'), (1, 'PREPARANDO'), (2, 'PRONTO'))
+
+class Pedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.SmallIntegerField(choices=ORDER_STATUS)
+    data = models.DateTimeField(auto_now=True)
+    valor = models.FloatField(null=False)
+
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    cliente = models.CharField(default=None)
