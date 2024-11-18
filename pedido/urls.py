047bee0a38d8e6ef364ddec9c8328bc5a2acfb3b
DjoninhaA <djonathan8016@gmail.comgit>
2024-11-18 20:26:18 -0300

    feat: criando metodos em pedidos

--- pedido/urls.py
+++ pedido/urls.py
@@ -3,5 +3,7 @@ from pedido import views
 from .views import pedido_create
 
 urlpatterns = [
-    path('ver/', views.pedido_create, name = 'pedidos.html'),
+    path('ver/', views.listar_pedidos, name = 'pedidos'),
+    path('criar/', views.pedido_create, name=  'criar'),
+    path('deletar/<int:pedido_id>/', views.deletar_pedido, name = 'deletar')
 ]
